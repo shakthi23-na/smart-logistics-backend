08099f643b2ef4b0a2c4b7f80cfc6a4718cdef63
shakthi23-na <23suca19@tcart.in>
2026-03-21 18:07:23 +0530

    Refactor: Remove unique constraint from trackingNumber column
    - Removed unique=true from @Column annotation on trackingNumber field
    - Allows more flexible duplicate handling at application level

--- src/main/java/com/smartlogistics/smart_logistics_backend/model/Shipment.java
+++ src/main/java/com/smartlogistics/smart_logistics_backend/model/Shipment.java
@@ -20,7 +20,7 @@ public class Shipment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String trackingNumber;
 
     @Column(nullable = false)
